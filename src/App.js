0c9288c60b72188e9e03d7a62edd41db538890fe
nonokaM <c1201071@gmail.com>
2023-04-10 23:30:38 +0900

    ルーティング

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Header from './components/HeaderComponent/Header';
 import HomePage from './components/HomePageComponent/HomePage';
+import Skill from './components/SkillComponent/Skill';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/skill" element={<Skill />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/production" element={<Production />} />
         </Routes>
       </div>
     </Router>
